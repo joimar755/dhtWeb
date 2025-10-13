df02e3db701906317185e1849f6c6ac0967f392e
joimar755 <joimarjose19@gmail.com>
2025-10-13 04:10:50 -0500

    re

--- Controlador/auth.controller.js
+++ Controlador/auth.controller.js
@@ -1,44 +1,42 @@
-import bcrypt from 'bcrypt'
-import pool from '../db.js'
+import bcrypt from "bcrypt";
+import pool from "../db.js";
+import { createAccessToken } from "../middleware/auth.js";
 
 export const Register = async (req, res) => {
-    const { username, password } = req.body;
-    //console.log(username,password);
+  const { username, password } = req.body;
+  //console.log(username,password);
 
-    try {
-        const haspassword = await bcrypt.hash(password, 10);
-        console.log(haspassword);
-        const [resultado] = await pool.query("INSERT INTO login(username,password) VALUES (?,?)",
-            [username, haspassword]
-        );
+  try {
+    const haspassword = await bcrypt.hash(password, 10);
+    console.log(haspassword);
+    const [resultado] = await pool.query(
+      "INSERT INTO login(username,password) VALUES (?,?)",
+      [username, haspassword]
+    );
 
-        const [rows] = await pool.query(
-            "SELECT id, username, fecha FROM login WHERE id = ?",
-            [resultado.insertId]
-        );
-        console.log(rows[0]);
-        res.json({
-            user: rows[0]
-        });
-    } catch (error) {
-        if (error.code === 'ER_DUP_ENTRY' || error.errno === 1062) {
-            res.json({
-                message: 'username ya está registrado'
-            });
-
-        }
+    const [rows] = await pool.query(
+      "SELECT id, username, fecha FROM login WHERE id = ?",
+      [resultado.insertId]
+    );
+    const token = await createAccessToken({ id: resultado.insertId }, process.env.JWT_SECRETO, {
+      expiresIn: "2h",
+    });
+    console.log(token);
+    return res.json({
+      user: rows[0],
+      token: token,
+    });
+  } catch (error) {
+    if (error.code === "ER_DUP_ENTRY" || error.errno === 1062) {
+      res.json({
+        message: "username ya está registrado",
+      });
     }
+  }
+};
 
-}
+export const Login = (req, res) => {};
 
-export const Login = (req, res) => {
+export const Cerrar_session = (req, res) => {};
 
-}
-
-export const Cerrar_session = (req, res) => {
-
-}
-
-export const Profile = (req, res) => {
-
-}
+export const Profile = (req, res) => {};
