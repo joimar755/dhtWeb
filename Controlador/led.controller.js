0466bff6636d0cbe6a59b27e02dedcd67dc4b595
joimar755 <joimarjose19@gmail.com>
2025-10-13 19:36:29 -0500

    leds

--- Controlador/led.controller.js
+++ Controlador/led.controller.js
@@ -1,15 +1,26 @@
 import { ledStatus } from "../server.js";
+import pool from "../db.js"
 
-export const get_leds = (req, res,) => {
+export const get_leds = (req, res) => {
     res.json(ledStatus);
 }
 
-export const post_leds = (req, res) => {
-    const { led1, led2, led3 } = req.body;
-    ledStatus.led1 = led1;
-    ledStatus.led2 = led2;
-    ledStatus.led3 = led3;
-    //io.emit("control", ledStatus);
-    console.log("Nuevo estado:", { ledStatus });
-    return res.json({ success: true, status: ledStatus });
+export const post_leds = async (req, res) => {
+    try {
+        const { led1, led2, led3 } = req.body;
+        if (led1 !== undefined) ledStatus.led1 = led1;
+        if (led2 !== undefined) ledStatus.led2 = led2;
+        if (led3 !== undefined) ledStatus.led3 = led3;
+        const [resultado] = await pool.query("INSERT INTO bombillo(led1, led2, led3, user_id) VALUES (?,?,?,?)",
+            [led1, led2, led3, req.userId]
+        )
+        console.log(req.userId);
+        //io.emit("control", ledStatus);
+        console.log("Nuevo estado:", { ledStatus });
+        return res.json({ success: true, status: ledStatus, userID: req.userId });
+    } catch (error) {
+        if (!res.headersSent) {
+            return res.status(500).json({ error: "Error interno" });
+        }
+    }
 }
